64e1a381e0d5b9e4f1b3f62997e0565f86bff8d4
lix <lix@paulian.net>
2016-04-01 14:35:37 +0200

    Added gnuarmeclipse specific files, as well as the vectors file for the em32fg series.

---
+++ submodules/CMSIS/Include/cmsis_device.h
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2015 Liviu Ionescu.
+// This file is part of the xPacks project (https://xpacks.github.io).
+// Modified for Silicon Labs EFR32FG family by LNP
+//
+
+#ifndef CMSIS_DEVICE_H_
+#define CMSIS_DEVICE_H_
+
+#include "em_device.h"
+
+#endif // CMSIS_DEVICE_H_
